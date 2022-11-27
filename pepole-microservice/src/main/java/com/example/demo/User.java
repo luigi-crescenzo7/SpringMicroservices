a1be235e7bf599d916f3de8c308a25234da52fb0
luigi <luigi.crescenzo.7@hotmail.it>
2022-11-27 12:03:35 +0100

    added more fields to User.java

--- pepole-microservice/src/main/java/com/example/demo/User.java
+++ pepole-microservice/src/main/java/com/example/demo/User.java
@@ -5,6 +5,8 @@ import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.time.LocalDate;
+
 @Document("User")
 @Data
 public class User {
@@ -12,4 +14,9 @@ public class User {
     @MongoId
     private String id;
     private String name;
+    private String surname;
+    private String email;
+    private String gender;
+    private String password;
+    private LocalDate dateOfBirth;
 }
