c9962439b593b6b301eaa101315a974dc566313d
luigi <luigi.crescenzo.7@hotmail.it>
2022-11-29 12:19:07 +0100

    implemented authentication with password hashing

--- pepole-microservice/src/main/java/com/example/demo/UserController.java
+++ pepole-microservice/src/main/java/com/example/demo/UserController.java
@@ -2,19 +2,25 @@ package com.example.demo;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
 
-    UserRepository userRepository;
+    final UserRepository userRepository;
+    final PasswordEncoder encoder;
 
-    public UserController(final UserRepository userRepository) {
+    public UserController(final UserRepository userRepository, final PasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
     @GetMapping("/all")
@@ -27,5 +33,23 @@ public class UserController {
     public List<User> getUsersByName(@RequestParam(name = "name") String name) {
         log.info("/users/name endpoint");
         return userRepository.findAllByName(name);
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public ResponseEntity<String> authUser(@RequestParam(name = "email") String email,
+                                           @RequestParam(name = "passwordHash") String passwordHash) {
+
+        List<User> users = userRepository.findAllUsersWithEmailAndPassword();
+        log.info(users.toString());
+
+        Optional<User> userOptional = users.stream().filter(
+                (user -> encoder.matches(passwordHash, user.getPassword()))).findFirst();
+
+        if (userOptional.isEmpty())
+            return new ResponseEntity<>("not authorized", HttpStatus.OK);
+
+        log.info("User is present");
+        return new ResponseEntity<>("authorized", HttpStatus.OK);
     }
 }
--- pepole-microservice/src/main/java/com/example/demo/UserRepository.java
+++ pepole-microservice/src/main/java/com/example/demo/UserRepository.java
@@ -6,10 +6,18 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     @Query("{name: '?0'}")
     List<User> findAllByName(@Param("name") String name);
+
+    @Query("{email:  '?0', password:  '?1'}")
+    Optional<User> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+    //change query to fetch only the password field
+    @Query("{}{_id: 0, email:1, password: 1}")
+    List<User> findAllUsersWithEmailAndPassword();
 }
