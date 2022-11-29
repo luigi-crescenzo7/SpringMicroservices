e71e7c0c3a6f6db940acf25196c71229051e0787
luigi <luigi.crescenzo.7@hotmail.it>
2022-11-29 12:16:15 +0100

    added Bcrypt dependency and Bean definition

--- src/main/java/it/unisa/tirocinio/TirocinioWebAppApplication.java
+++ src/main/java/it/unisa/tirocinio/TirocinioWebAppApplication.java
@@ -3,9 +3,16 @@ package it.unisa.tirocinio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TirocinioWebAppApplication {
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TirocinioWebAppApplication.class, args);
