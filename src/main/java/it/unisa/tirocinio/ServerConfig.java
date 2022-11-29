650cdd4ccfd073de92a01ff83c4cef1d35330d29
luigi <luigi.crescenzo.7@hotmail.it>
2022-11-29 20:27:25 +0100

    added traffic redirection from http to https

---
+++ src/main/java/it/unisa/tirocinio/ServerConfig.java
@@ -0,0 +1,43 @@
+package it.unisa.tirocinio;
+
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+                super.postProcessContext(context);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(getHttpConnector());
+        return factory;
+    }
+
+    private Connector getHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
