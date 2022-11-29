432fc85022447f6e68e096f9387731ed2b373573
luigi <luigi.crescenzo.7@hotmail.it>
2022-11-29 12:18:03 +0100

    modified UserServiceImpl and UserService

--- src/main/java/it/unisa/tirocinio/services/UserService.java
+++ src/main/java/it/unisa/tirocinio/services/UserService.java
@@ -2,4 +2,6 @@ package it.unisa.tirocinio.services;
 
 public interface UserService {
     String getPersons();
+
+    boolean login(String email, String password);
 }
--- src/main/java/it/unisa/tirocinio/services/UserServiceImpl.java
+++ src/main/java/it/unisa/tirocinio/services/UserServiceImpl.java
@@ -3,8 +3,10 @@ package it.unisa.tirocinio.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -12,13 +14,15 @@ import java.util.Optional;
 // SpringBoot REST MongoDB Endpoint
 public class UserServiceImpl implements UserService {
 
-    private final String URL = "http://localhost:8081/persons";
+    private final String URL = "http://localhost:8081/users";
     private final String URI = "/all";
+
+    private final String LOGIN = "/login";
 
     @Override
     public String getPersons() {
         log.info("Querying microservice endpoint...");
-        final WebClient client = WebClient.builder().baseUrl(URL).build();
+        final WebClient client = buildWebClient();
 
         Optional<String> opt = client.get()
                 .uri(URI)
@@ -26,5 +30,22 @@ public class UserServiceImpl implements UserService {
                 .bodyToMono(String.class).blockOptional();
 
         return opt.orElse(null);
+    }
+
+    @Override
+    public boolean login(String email, String password) {
+        final WebClient client = buildWebClient();
+
+        Optional<String> opt = client.post().uri(LOGIN)
+                .body(BodyInserters.fromFormData("email", email).with("passwordHash", password))
+                .retrieve()
+                .bodyToMono(String.class).blockOptional();
+
+        return opt.map(s -> s.equalsIgnoreCase("authorized")).orElse(false);
+    }
+
+
+    private WebClient buildWebClient() {
+        return WebClient.builder().baseUrl(URL).build();
     }
 }
