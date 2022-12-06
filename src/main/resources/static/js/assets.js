7642f7594d9d1cc5cb8f8aba204f9955e4e87d23
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-06 16:06:30 +0100

    refactor

--- src/main/resources/static/js/assets.js
+++ src/main/resources/static/js/assets.js
@@ -1,16 +1,6 @@
-/*fetch("./js/config.json")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error))*/
-
 function fetch_assets() {
-    const windowUrl = window.location.host;
-    let url = 'http://localhost:8087/api/contract/all';
-    if (!windowUrl.includes('localhost')) {
-        url = 'http://host.minikube.internal:8087/api/contract/all'
-    }
     const pTag = document.getElementById('data');
-    fetch(url)
+    fetch('https://localhost:8443/api/contract/all')
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
