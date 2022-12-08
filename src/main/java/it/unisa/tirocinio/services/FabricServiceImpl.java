ca8fd091d3c3b034b6c3b2aa02fbb1a7f742aca3
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-08 21:39:19 +0100

    removed commented code, refactor

--- src/main/java/it/unisa/tirocinio/services/FabricServiceImpl.java
+++ src/main/java/it/unisa/tirocinio/services/FabricServiceImpl.java
@@ -1,16 +1,10 @@
 package it.unisa.tirocinio.services;
 
 
-import io.netty.channel.epoll.Epoll;
-import io.netty.channel.epoll.EpollDatagramChannel;
-import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.resolver.DefaultAddressResolverGroup;
-import io.netty.resolver.dns.DnsAddressResolverGroup;
-import io.netty.resolver.dns.DnsNameResolverBuilder;
-import io.netty.resolver.dns.DnsServerAddressStreamProviders;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -19,7 +13,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 import javax.net.ssl.SSLException;
-import java.net.InetAddress;
 import java.util.Optional;
 
 
@@ -34,7 +27,7 @@ public class FabricServiceImpl implements FabricService {
     public String findAllAssets() {
         log.info("Querying fabric service...");
 
-        WebClient client = buildWebClient();
+        WebClient client = httpsWebClient();
         Optional<String> opt = client.get()
                 .uri("/api/contract/all").retrieve()
                 .bodyToMono(String.class).blockOptional();
@@ -42,7 +35,11 @@ public class FabricServiceImpl implements FabricService {
         return opt.orElse(null);
     }
 
-    private WebClient buildWebClient() {
+    private WebClient httpWebClient() {
+        return WebClient.builder().baseUrl(FABRIC_URL).build();
+    }
+
+    private WebClient httpsWebClient() {
         SslContext sslContext;
         try {
             sslContext = SslContextBuilder.forClient()
@@ -52,12 +49,6 @@ public class FabricServiceImpl implements FabricService {
             throw new RuntimeException(e);
         }
 
-        /*try (DnsAddressResolverGroup group = new DnsAddressResolverGroup(new DnsNameResolverBuilder()
-                .channelType(EpollDatagramChannel.class)
-                .ttl(120, 120)
-                .negativeTtl(60)
-                .nameServerProvider(DnsServerAddressStreamProviders.platformDefault()))) {*/
-
         HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE)
                 .secure(provider -> provider.sslContext(sslContext));
 
@@ -65,7 +56,8 @@ public class FabricServiceImpl implements FabricService {
         String local = "https://localhost:8443/";
 
         return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient)).baseUrl(FABRIC_URL.contains("https") ? FABRIC_URL : local).build();
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .baseUrl(FABRIC_URL.contains("https") ? FABRIC_URL : local).build();
 
 
     }
