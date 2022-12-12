0276c4131d4d90c4a804e29f9260032e191e8f41
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-12 14:51:24 +0100

    moved UserRepository

--- kotlin-service/src/main/kotlin/com/example/kotlinservice/repositories/UserRepository.kt
+++ kotlin-service/src/main/kotlin/com/example/kotlinservice/repositories/UserRepository.kt
@@ -1,5 +1,6 @@
-package com.example.kotlinservice
+package com.example.kotlinservice.repositories
 
+import com.example.kotlinservice.beans.User
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
