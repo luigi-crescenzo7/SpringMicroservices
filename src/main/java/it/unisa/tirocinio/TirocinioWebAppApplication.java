262d558fc36d14b8f50d5014fa2f96328187ea94
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-16 12:47:11 +0100

    removed PasswordEncode bean

--- src/main/java/it/unisa/tirocinio/TirocinioWebAppApplication.java
+++ src/main/java/it/unisa/tirocinio/TirocinioWebAppApplication.java
@@ -3,18 +3,10 @@ package it.unisa.tirocinio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableConfigurationProperties
 @SpringBootApplication
 public class TirocinioWebAppApplication {
-    @Bean
-    public PasswordEncoder bcryptPasswordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(TirocinioWebAppApplication.class, args);
     }
