3d0ae18d6979305008300eefb14ecafb5cfe60ea
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-18 10:28:49 +0100

    added converter

--- kotlin-service/src/main/kotlin/com/example/kotlinservice/VaultItemWriteConverter.kt
+++ kotlin-service/src/main/kotlin/com/example/kotlinservice/VaultItemWriteConverter.kt
@@ -1,30 +1,34 @@
 package com.example.kotlinservice
 
-import com.example.kotlinservice.controllers.VaultItemDTO
-import com.mongodb.BasicDBObjectBuilder
-import com.mongodb.DBObject
+
+import com.example.kotlinservice.beans.VaultItem
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.WritingConverter
+import org.bson.Document
+import org.bson.types.ObjectId
 
 
+
+// non vi è necessità di un converter
+/*
 @WritingConverter
-class VaultItemWriteConverter : Converter<VaultItemDTO, DBObject> {
-    override fun convert(source: VaultItemDTO): DBObject {
+@Slf4j
+class VaultItemWriteConverter : Converter<VaultItem, Document> {
 
-        /*val dbo = BasicDBObject()
+    private val logger: Logger = LoggerFactory.getLogger(VaultItemWriteConverter::class.java)
 
-        with(source) {
-            dbo.put("_id", this.vaultItem.id+"")
-            dbo.put("itemName")
-        }*/
-
-        return BasicDBObjectBuilder.start()
-            .add("_id", source.vaultItem.id!!)
-            .add("itemName", source.vaultItem.itemName)
-            .add("creationDate", source.vaultItem.creationDate)
-            .add("resourceURI", source.vaultItem.resourceURI)
-            .add("idCardNumber", source.vaultItem.idCardNumber)
-            .add("ownerId", source.ownerId)
-            .get()
+    override fun convert(source: VaultItem): Document? {
+        val document = Document()
+        return with(document) {
+            this.append("_id", ObjectId())
+            this.append("idCardNumber", source.idCardNumber)
+            this.append("resourceURI", source.resourceURI)
+            this.append("ownerId", ObjectId(source.owner!!.id))
+            this.append("creationDate", source.creationDate)
+            this.append("itemName", source.itemName)
+        }
     }
-}
+}*/
