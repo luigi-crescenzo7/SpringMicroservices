c64b5d032eafa3aa5f475fccf9d8d7c9d444b06e
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-24 10:39:54 +0100

    added findAll method to VaultItemService, added custom deserializing method for VaultItem bean

--- src/main/java/it/unisa/tirocinio/services/VaultItemService.java
+++ src/main/java/it/unisa/tirocinio/services/VaultItemService.java
@@ -3,6 +3,9 @@ package it.unisa.tirocinio.services;
 
 import it.unisa.tirocinio.beans.VaultItem;
 
+import java.util.List;
+
 public interface VaultItemService {
     boolean saveItem(VaultItem item);
+    List<VaultItem> findAll();
 }
--- src/main/java/it/unisa/tirocinio/services/VaultItemServiceImpl.java
+++ src/main/java/it/unisa/tirocinio/services/VaultItemServiceImpl.java
@@ -1,31 +1,54 @@
 package it.unisa.tirocinio.services;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import it.unisa.tirocinio.beans.VaultItem;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @Slf4j
 public class VaultItemServiceImpl implements VaultItemService {
-
-    private final static String URI = "/save-item";
+    private final static ObjectMapper mapper = new ObjectMapper();
+    private final static String CREATE_URI = "/save-item";
+    private final static String ALL_URI = "/items";
     private final static String ENDPOINT = "http://localhost:8082/kotlin";
 
     @Override
     public boolean saveItem(VaultItem item) {
         WebClient client = httpWebClient();
 
-        Optional<String> optional = client.post().uri(URI)
+        Optional<String> optional = client.post().uri(CREATE_URI)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(item)
                 .retrieve()
                 .bodyToMono(String.class).blockOptional();
         String restResult = optional.orElseThrow();
         return restResult.equals("true");
+    }
+
+    @Override
+    public List<VaultItem> findAll() {
+        WebClient client = httpWebClient();
+
+        Optional<String> opt = client.get()
+                .uri(ALL_URI).retrieve()
+                .bodyToMono(String.class).blockOptional();
+
+        String result = opt.orElseThrow();
+        try {
+            return mapper.readerForListOf(VaultItem.class).readValue(result);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
