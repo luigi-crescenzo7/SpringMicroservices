cbf576b1783c303c717e4e12217c4a6fbe50c1e4
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-25 18:26:21 +0100

    added findAssetsByOwnerId method with implementation for fetching filtered items from fabric rest gateway

--- src/main/java/it/unisa/tirocinio/services/FabricService.java
+++ src/main/java/it/unisa/tirocinio/services/FabricService.java
@@ -3,8 +3,12 @@ package it.unisa.tirocinio.services;
 
 import it.unisa.tirocinio.beans.IdCardItem;
 
+import java.util.List;
+
 public interface FabricService {
     String findAllAssets();
+
+    List<IdCardItem> findAssetsByOwnerId(String ownerId);
 
     String saveItem(IdCardItem item);
 }
--- src/main/java/it/unisa/tirocinio/services/FabricServiceImpl.java
+++ src/main/java/it/unisa/tirocinio/services/FabricServiceImpl.java
@@ -1,6 +1,7 @@
 package it.unisa.tirocinio.services;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.handler.ssl.SslContext;
@@ -17,6 +18,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 import javax.net.ssl.SSLException;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -26,7 +28,10 @@ public class FabricServiceImpl implements FabricService {
     private final static ObjectMapper mapper = new ObjectMapper();
 
     @Value("${app.fabric-rest-url}")
-    private String FABRIC_URL;
+    private String FABRIC_ENDPOINT;
+    private static final String ALL_URI = "/api/contract/all";
+    private static final String SAVE_URI = "/api/contract/save";
+    private static final String ASSETS_BY_OWNER_ID = "/api/contract/ownerId";
 
     @Override
     public String findAllAssets() {
@@ -34,10 +39,28 @@ public class FabricServiceImpl implements FabricService {
 
         WebClient client = httpsWebClient();
         Optional<String> opt = client.get()
-                .uri("/api/contract/all").retrieve()
+                .uri(ALL_URI).retrieve()
                 .bodyToMono(String.class).blockOptional();
 
         return opt.orElse(null);
+    }
+
+    @Override
+    public List<IdCardItem> findAssetsByOwnerId(String ownerId) {
+        log.info("find assets by owner id");
+        JsonNode node = mapper.createObjectNode().put("ownerId", ownerId);
+        WebClient client = httpsWebClient();
+        Optional<String> optResult = client.post().uri(ASSETS_BY_OWNER_ID)
+                .bodyValue(node).retrieve().bodyToMono(String.class).blockOptional();
+        String result = optResult.orElseThrow();
+
+        log.info("Result: " + result);
+
+        try {
+            return mapper.readerForListOf(IdCardItem.class).readValue(result);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -45,16 +68,15 @@ public class FabricServiceImpl implements FabricService {
         JsonNode node = mapper.createObjectNode().set("asset", mapper.valueToTree(item));
         WebClient client = httpsWebClient();
         Optional<String> opt = client.post()
-                .uri("/api/contract/create")
+                .uri(SAVE_URI)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(node)
-                //.bodyValue(new IdCardItemDTO(item))
                 .retrieve().bodyToMono(String.class).blockOptional();
         return opt.orElse(null);
     }
 
     private WebClient httpWebClient() {
-        return WebClient.builder().baseUrl(FABRIC_URL).build();
+        return WebClient.builder().baseUrl(FABRIC_ENDPOINT).build();
     }
 
     private WebClient httpsWebClient() {
@@ -69,12 +91,11 @@ public class FabricServiceImpl implements FabricService {
         HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE)
                 .secure(provider -> provider.sslContext(sslContext));
 
-        log.info(FABRIC_URL);
         String local = "https://localhost:8447/";
 
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .baseUrl(FABRIC_URL.contains("https") ? FABRIC_URL : local).build();
+                .baseUrl(FABRIC_ENDPOINT.contains("https") ? FABRIC_ENDPOINT : local).build();
     }
 }
 
