ed785e80f35ff115e2021bac7f794a44fce6341e
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-25 18:40:09 +0100

    removed assets.js file, refactor index.html

--- src/main/resources/static/js/assets.js
+++
@@ -1,9 +0,0 @@
-function fetch_assets() {
-    const pTag = document.getElementById('data');
-    fetch('https://localhost:8443/api/contract/all')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            pTag.textContent = JSON.stringify(data);
-        }).catch((error) => console.error(error))
-}
