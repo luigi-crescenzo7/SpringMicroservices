96e6e0790d783600b3a65802c49d1be0c1bbba7d
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-30 16:16:47 +0100

    added ObjectMapper bean with registered JavaTimeModule for LocalDate serialization

--- src/main/java/it/unisa/tirocinio/TirocinioWebAppApplication.java
+++ src/main/java/it/unisa/tirocinio/TirocinioWebAppApplication.java
@@ -1,12 +1,19 @@
 package it.unisa.tirocinio;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @EnableConfigurationProperties
 @SpringBootApplication
 public class TirocinioWebAppApplication {
+    @Bean
+    public ObjectMapper mapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
     public static void main(String[] args) {
         SpringApplication.run(TirocinioWebAppApplication.class, args);
     }
