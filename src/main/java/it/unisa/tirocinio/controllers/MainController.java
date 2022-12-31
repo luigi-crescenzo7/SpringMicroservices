b3060111c7d5bb9f0e8058f5557981cf56cb8694
luigi <luigi.crescenzo.7@hotmail.it>
2022-12-31 13:27:16 +0100

    removed users() method

--- src/main/java/it/unisa/tirocinio/controllers/MainController.java
+++ src/main/java/it/unisa/tirocinio/controllers/MainController.java
@@ -4,9 +4,7 @@ package it.unisa.tirocinio.controllers;
 import it.unisa.tirocinio.beans.User;
 import it.unisa.tirocinio.services.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -38,11 +36,5 @@ public class MainController {
     @GetMapping(value = "/login")
     public String login() {
         return "login";
-    }
-
-    @GetMapping(value = "/users")
-    public ResponseEntity<String> users() {
-        log.info("Call to /users endpoint");
-        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 }
