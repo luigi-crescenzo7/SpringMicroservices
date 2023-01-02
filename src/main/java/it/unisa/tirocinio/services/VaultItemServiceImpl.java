ff41dde04b9aa468436bd3ebc885fee2c4de5e00
luigi <luigi.crescenzo.7@hotmail.it>
2023-01-02 20:34:26 +0100

    refactor http webclient calls, added http error handling, added field injection

--- src/main/java/it/unisa/tirocinio/services/VaultItemServiceImpl.java
+++ src/main/java/it/unisa/tirocinio/services/VaultItemServiceImpl.java
@@ -1,58 +1,56 @@
 package it.unisa.tirocinio.services;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import it.unisa.tirocinio.beans.VaultItem;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @Slf4j
 public class VaultItemServiceImpl implements VaultItemService {
-    private final static ObjectMapper mapper = new ObjectMapper();
+    @Value("${app.vaultitems-service-url}")
+    private String ENDPOINT;
     private final static String CREATE_URI = "/save-item";
     private final static String ALL_URI = "/items";
-    private final static String ENDPOINT = "http://localhost:8082/kotlin";
 
     @Override
     public boolean saveItem(VaultItem item) {
         WebClient client = httpWebClient();
 
-        Optional<String> optional = client.post().uri(CREATE_URI)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(item)
-                .retrieve()
-                .bodyToMono(String.class).blockOptional();
-        String restResult = optional.orElseThrow();
-        return restResult.equals("true");
+        Optional<ResponseEntity<String>> optional = client.post().uri(CREATE_URI)
+                .contentType(MediaType.APPLICATION_JSON).bodyValue(item)
+                .retrieve().onStatus(HttpStatusCode::isError, clientResponse ->
+                        clientResponse.toEntity(String.class).map(CustomResponseException::new))
+                .toEntity(String.class).blockOptional();
+
+        return optional.map(entity -> Objects.equals(entity.getBody(), "true")).orElseThrow();
     }
 
     @Override
     public List<VaultItem> findAll() {
         WebClient client = httpWebClient();
 
-        Optional<String> opt = client.get()
-                .uri(ALL_URI).retrieve()
-                .bodyToMono(String.class).blockOptional();
+        Optional<ResponseEntity<List<VaultItem>>> opt = client.get().uri(ALL_URI)
+                .retrieve().onStatus(HttpStatusCode::isError, clientResponse ->
+                        clientResponse.toEntity(String.class).map(CustomResponseException::new))
+                .toEntityList(VaultItem.class).blockOptional();
 
-        String result = opt.orElseThrow();
-        try {
-            return mapper.readerForListOf(VaultItem.class).readValue(result);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return opt.map(HttpEntity::getBody).orElseThrow();
     }
 
 
     private WebClient httpWebClient() {
-        return WebClient.builder().baseUrl(ENDPOINT).build();
+        return WebClient.builder().baseUrl(ENDPOINT + "kotlin/").build();
     }
 }
