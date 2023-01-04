242c1b800e2e9514bf0fef9c44bb5ff8274fda88
luigi <luigi.crescenzo.7@hotmail.it>
2023-01-04 16:32:48 +0100

    refactoring

--- kotlin-service/src/main/kotlin/com/example/kotlinservice/beans/VaultItem.kt
+++ kotlin-service/src/main/kotlin/com/example/kotlinservice/beans/VaultItem.kt
@@ -2,38 +2,26 @@ package com.example.kotlinservice.beans
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
-import lombok.Data
-import lombok.NoArgsConstructor
-import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.DocumentReference
-import org.springframework.data.mongodb.core.mapping.Field
-import org.springframework.data.mongodb.core.mapping.MongoId
+import org.springframework.data.mongodb.core.mapping.*
 import java.time.LocalDate
 
-@Data
+
 @Document("VaultItem")
-@NoArgsConstructor
-class VaultItem(
-    @MongoId
+data class VaultItem(
+    @MongoId(FieldType.OBJECT_ID)
     @Field
-    @field:JsonProperty var id: String?, @Field
-    @field:JsonProperty var idCardNumber: String, resourceURI: String, ownerId: User, @Field
-    @field:JsonProperty var itemName: String, creationDate: LocalDate
+    @field:JsonProperty
+    var id: String,
+    @Field
+    @field:JsonProperty var idCardNumber: String,
+    @Field @field:JsonProperty var resourceURI: String,
+    @DocumentReference(collection = "User") @Field(value = "ownerId") @field:JsonProperty(value = "ownerId")
+    var user: User,
+    @Field
+    @field:JsonProperty var itemName: String,
+    @Field @field:JsonProperty @field:JsonFormat(shape = JsonFormat.Shape.STRING) var creationDate: LocalDate
 ) {
 
-    @Field
-    @field:JsonProperty
-    var resourceURI: String? = resourceURI
-
-    @DocumentReference(collection = "User")
-    @Field("ownerId")
-    @field:JsonProperty(value = "ownerId")
-    var ownerId: User? = ownerId
-
-    @Field
-    @field:JsonProperty
-    @field:JsonFormat(shape = JsonFormat.Shape.STRING)
-    var creationDate: LocalDate? = creationDate
-
+    constructor(): this("", "", "", User(""), "", LocalDate.now())
 
 }
