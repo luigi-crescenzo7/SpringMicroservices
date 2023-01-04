e62dff8d19bd76202c2efeb4adc9ceb34fe729c8
luigi <luigi.crescenzo.7@hotmail.it>
2023-01-04 16:38:16 +0100

    added HttpSession user handling, added /find-by-id endpoint

--- src/main/java/it/unisa/tirocinio/controllers/VaultItemController.java
+++ src/main/java/it/unisa/tirocinio/controllers/VaultItemController.java
@@ -2,16 +2,14 @@ package it.unisa.tirocinio.controllers;
 
 import it.unisa.tirocinio.beans.VaultItem;
 import it.unisa.tirocinio.services.VaultItemService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,11 +32,15 @@ public class VaultItemController {
     }
 
     @PostMapping("/save-vault-item")
-    public ResponseEntity<String> item(@ModelAttribute VaultItem item, BindingResult result) {
+    public ResponseEntity<String> item(@ModelAttribute VaultItem item, BindingResult result, HttpSession session) {
         if (result.hasErrors())
             return new ResponseEntity<>("Error mapping VaultItem object", HttpStatus.BAD_REQUEST);
+        String uId = (String) session.getAttribute("user");
 
-        item.setOwnerId("63834344af957a1c25218b32");
+        if(uId == null)
+            return new ResponseEntity<>("user id not found in http session", HttpStatus.BAD_REQUEST);
+
+        item.setOwnerId(uId);
 
         boolean flag = vaultItemService.saveItem(item);
         if (!flag) return new ResponseEntity<>("item not saved", HttpStatus.BAD_REQUEST);
@@ -46,6 +48,17 @@ public class VaultItemController {
         log.info("Item received: " + item);
         return new ResponseEntity<>("item saved", HttpStatus.OK);
     }
+
+    @GetMapping(value = "/find-by-id")
+    public ResponseEntity<List<VaultItem>> itemsByOwnerId(HttpSession session) {
+        String userId = (String) session.getAttribute("user");
+        if (userId == null) {
+            log.info("User id not found in session object");
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(vaultItemService.findAllById(userId), HttpStatus.OK);
+    }
+
     @GetMapping(value = "/vault-items")
     public ResponseEntity<List<VaultItem>> items() {
         List<VaultItem> items = vaultItemService.findAll();
