db53b5c9edbed78dc59cbcca33a0bb7166ea48e9
luigi <luigi.crescenzo.7@hotmail.it>
2023-01-06 13:02:00 +0100

    refactor, added Http session handling

--- src/main/java/it/unisa/tirocinio/controllers/IdCardController.java
+++ src/main/java/it/unisa/tirocinio/controllers/IdCardController.java
@@ -2,7 +2,9 @@ package it.unisa.tirocinio.controllers;
 
 
 import it.unisa.tirocinio.beans.IdCardItem;
+import it.unisa.tirocinio.services.CustomResponseException;
 import it.unisa.tirocinio.services.FabricService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,27 +35,35 @@ public class IdCardController {
     @GetMapping(value = "/assets")
     public String assets(Model model) {
         log.info("/assets");
-        List<IdCardItem> result = fabricService.findAllAssets();
-        model.addAttribute("assets", result);
+        List<IdCardItem> assets = fabricService.findAllAssets();
+        model.addAttribute("assets", assets);
         return "assetsTemplate";
     }
 
     @GetMapping("/ownerId")
-    public String assetsByOwnerId(Model model) {
-        List<IdCardItem> items = fabricService.findAssetsByOwnerId("ownerId1");
-        model.addAttribute("items", items);
-        return "show-items";
+    public String assetsByOwnerId(Model model, HttpSession session) {
+        String userId = (String) session.getAttribute("user");
+        if(userId == null ) {
+            throw new CustomResponseException(
+                    new ResponseEntity<>("no user id found", HttpStatus.BAD_REQUEST));
+        }
+        List<IdCardItem> items = fabricService.findAssetsByOwnerId(userId);
+        model.addAttribute("assets", items);
+        return "assetsTemplate";
     }
 
     @PostMapping(value = "/save-card-item")
-    public ResponseEntity<String> card(@ModelAttribute IdCardItem item) {
-
-        item.setOwnerId("ownerId1");
+    public ResponseEntity<String> card(@ModelAttribute IdCardItem item, HttpSession session) {
+        String userId = (String) session.getAttribute("user");
+        if(userId == null ) {
+            throw new CustomResponseException(
+                    new ResponseEntity<>("no user id found", HttpStatus.BAD_REQUEST));
+        }
+        item.setOwnerId(userId);
         IdCardItem result = fabricService.saveItem(item);
         if (result == null)
             return new ResponseEntity<>("Error saving item", HttpStatus.BAD_REQUEST);
 
-        log.info("Result: " + result);
         return new ResponseEntity<>("Saved item: "+result.getId(), HttpStatus.OK);
     }
 }
