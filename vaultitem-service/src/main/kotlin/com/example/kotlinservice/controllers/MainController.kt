b9bfa2654bde1d823b57757e8a5d8e10ed2f20e5
luigi <luigi.crescenzo.7@hotmail.it>
2023-01-07 10:43:15 +0100

    added /delete-item & /update-item endpoints for MainController.kt in vault service

--- vaultitem-service/src/main/kotlin/com/example/kotlinservice/controllers/MainController.kt
+++ vaultitem-service/src/main/kotlin/com/example/kotlinservice/controllers/MainController.kt
@@ -21,8 +21,8 @@ inline fun <reified T> getLogger(): Logger {
 }
 
 
-@RestController
 @Slf4j
+@RestController
 @RequestMapping("/kotlin")
 class MainController(
     private val repository: UserRepository,
@@ -44,12 +44,44 @@ class MainController(
         return repository.findAll()
     }
 
+
+    @PostMapping("/delete-item")
+    fun delete(@RequestBody id: String): ResponseEntity<String> {
+        val itemId = mapper.readTree(id).get("id").asText()
+        val objId = ObjectId(itemId)
+        val opt = vaultItemRepository.findById(objId)
+
+        if(opt.isEmpty)
+            return ResponseEntity<String>("item not found with id $itemId", HttpStatus.BAD_REQUEST)
+
+        vaultItemRepository.deleteById(objId)
+        return ResponseEntity<String>("true", HttpStatus.OK)
+    }
+
+    @PostMapping("/update-item")
+    fun update(@RequestBody item: VaultItemDTO): ResponseEntity<String> {
+        log.info(mapper.writeValueAsString(item))
+        val opt = vaultItemRepository.findById(ObjectId(item.id))
+
+        if(opt.isEmpty)
+            return ResponseEntity<String>("item not found with id ${item.id}", HttpStatus.BAD_REQUEST)
+
+        val itemToUpdate = VaultItem(ObjectId(item.id),
+                                    item.idCardNumber,
+                                    item.resourceURI,
+                                    User(item.ownerId),
+                                    item.itemName,
+                                    item.creationDate)
+
+        val updatedItem = vaultItemRepository.save(itemToUpdate)
+        return ResponseEntity<String>("${updatedItem.id != null}", HttpStatus.OK)
+    }
     @PostMapping("/save-item")
     fun item(@RequestBody item: VaultItemDTO): ResponseEntity<String> {
         log.info("item: $item")
         val user = User(item.ownerId)
 
-        val savedItem = vaultItemRepository.insert(
+        val savedItem = vaultItemRepository.save(
             VaultItem(
                 null,
                 item.idCardNumber,
@@ -70,7 +102,6 @@ class MainController(
     }
 
     @GetMapping("/test")
-    @ResponseBody
     fun test(): ResponseEntity<String> {
         log.info("/test endpoint")
         return ResponseEntity<String>("Hello\n", HttpStatus.OK)
