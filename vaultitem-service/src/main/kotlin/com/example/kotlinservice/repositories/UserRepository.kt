26f866658ef9f29787172110942fc46dc47cdc31
luigi <luigi.crescenzo.7@hotmail.it>
2023-01-08 18:16:30 +0100

    removed unused classes

--- vaultitem-service/src/main/kotlin/com/example/kotlinservice/repositories/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinservice.repositories
-
-import com.example.kotlinservice.beans.User
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface UserRepository : MongoRepository<User, String>
