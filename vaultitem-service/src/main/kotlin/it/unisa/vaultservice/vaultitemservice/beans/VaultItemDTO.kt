d0c0287fb53b25ebba7aa6deb0b70e61560d301c
luigi <luigi.crescenzo.7@hotmail.it>
2023-07-19 09:43:51 +0200

    modified VaultItemDTO.kt

--- vaultitem-service/src/main/kotlin/it/unisa/vaultservice/vaultitemservice/beans/VaultItemDTO.kt
+++ vaultitem-service/src/main/kotlin/it/unisa/vaultservice/vaultitemservice/beans/VaultItemDTO.kt
@@ -1,18 +1,18 @@
 package it.unisa.vaultservice.vaultitemservice.beans
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import lombok.Data
-import lombok.NoArgsConstructor
+import lombok.*
 import java.time.LocalDate
+
 
 @Data
 @NoArgsConstructor
 class VaultItemDTO
     (
-    @field:JsonProperty var id: String?,
-    @field:JsonProperty var idCardNumber: String,
-    @field:JsonProperty var resourceURI: String,
-    @field:JsonProperty var itemName: String,
-    @field:JsonProperty var creationDate: LocalDate,
-    @field:JsonProperty var ownerId: String
+    @JsonProperty("id") var id: String?,
+    @JsonProperty("idCardNumber") var idCardNumber: String,
+    @JsonProperty("resourceURI") var resourceURI: String,
+    @JsonProperty("itemName") var itemName: String,
+    @JsonProperty("creationDate") var creationDate: LocalDate,
+    @JsonProperty("ownerId") var ownerId: String
 )
